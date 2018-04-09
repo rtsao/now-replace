977b5eddc372bf6ad8daadd0eddfb4b70c04badd
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-04-09 11:30:04 -0700

    Migrate to now.sh v2 API

--- bin/now-replace.js
+++ bin/now-replace.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const mri = require('mri');
+const fetch = require('node-fetch');
 
 const flags = mri(process.argv.slice(2), {
   alias: {
@@ -32,22 +33,49 @@ async function setAlias(aliasUrl, deploymentUrl, token) {
     }
   }
 
-  const NowClient = require('now-client');
-  now = new NowClient(token);
+  const authHeaders = {
+    Authorization: `Bearer: ${token}`
+  };
+
   deploymentUrl = normalizeUrl(deploymentUrl);
   aliasUrl = normalizeUrl(aliasUrl);
 
   async function getDeploymentId(deploymentUrl) {
-    const deployments = await now.getDeployments();
-    return deployments.find(({url}) => url === deploymentUrl).uid;
+    const req = await fetch('https://api.zeit.co/v2/now/deployments', {
+      headers: authHeaders
+    });
+    const json = await req.json();
+    return json.deployments.find(({url}) => url === deploymentUrl).uid;
   }
 
   async function getAliasedDeployment(aliasUrl) {
-    const aliases = await now.getAliases();
-    const found = aliases.find(({alias}) => alias === aliasUrl);
+    const req = await fetch('https://api.zeit.co/v2/now/deployments', {
+      headers: authHeaders
+    });
+    const json = await req.json();
+    const found = json.aliases.find(({alias}) => alias === aliasUrl);
     if (found) {
       return found.deployment;
     }
+  }
+  function createAlias(uid, aliasUrl) {
+    return fetch(`https://api.zeit.co/v2/now/deployments/${uid}/aliases`, {
+      method: 'POST',
+      body: JSON.stringify({
+        alias: aliasUrl
+      }),
+      headers: {
+        ...authHeaders,
+        'Contet-Type': 'application/json'
+      }
+    });
+  }
+
+  function deleteDeployment(uid) {
+    return fetch(`https://api.zeit.co/v2/now/deployments/${uid}`, {
+      method: 'DELETE',
+      headers: authHeaders
+    });
   }
 
   let uid;
@@ -66,14 +94,14 @@ async function setAlias(aliasUrl, deploymentUrl, token) {
   }
 
   try {
-    await now.createAlias(uid, aliasUrl);
+    await createAlias(uid, aliasUrl);
   } catch (err) {
     throw err;
   }
 
   if (existing) {
     try {
-      now.deleteDeployment(existing.id);
+      deleteDeployment(existing.id);
     } catch (err) {
       throw err;
     }
